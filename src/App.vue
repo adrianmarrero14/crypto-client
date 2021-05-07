f53edcc777ab08c446d7e891ac5d83a67b3cb6a4
Adrian Marrero <adrianmarrero18@gmail.com>
2021-05-07 11:06:08 -0400

    Routes Completed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <main>
-    <px-header/>
-    <px-assets-table/>
+    <px-header />
+    <router-view class="container px-5 sm:px-20 by-20 flex justify-center" />
   </main>
 </template>
 
 <script>
 import PxHeader from "@/components/PxHeader"
-import PxAssetsTable from "@/components/PxAssetsTable"
 
 export default {
   name: 'App',
   components: {
-    PxHeader, PxAssetsTable
+    PxHeader,
   }
 }
 </script>
